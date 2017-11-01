f8e88425244e58feae921dd621508d412f797089
ahmadali shafiee <mail@ahmadalli.net>
2017-11-01 23:18:37 +0330

    Add project files.

---
+++ YAPD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace YAPD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
